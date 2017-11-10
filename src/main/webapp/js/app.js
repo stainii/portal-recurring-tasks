09a22acc9e78418997c4b23a13a07a15d886363e
stijnhooft@hotmail.com <stijnhoof@hotmail.com>
2017-11-10 19:51:31 +0100

    add dummy form to add an execution

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,13 +1,25 @@
 import React from "react";
 import HousagotchiCreature from "./components/HousagotchiCreature/HousagotchiCreature";
 import AddExecution from "./components/AddExecution/AddExecution";
+import {Tab, TabList, TabPanel, Tabs} from 'react-tabs';
 
 class Housagotchi extends React.Component {
     render() {
         return (
             <div className="housagotchi-component">
                 <HousagotchiCreature/>
-                <AddExecution/>
+                <Tabs className="tabs">
+                    <TabList>
+                        <Tab>Add execution</Tab>
+                        <Tab>Manage tasks</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <AddExecution/>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Manage tasks: todo</h2>
+                    </TabPanel>
+                </Tabs>
             </div>
         );
     }
--- src/main/webapp/js/components/AddExecution/AddExecution.js
+++ src/main/webapp/js/components/AddExecution/AddExecution.js
@@ -1,12 +1,56 @@
 import React from "react";
 
+import moment from "moment";
+
 class AddExecution extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {task: '', date: moment().format("YYYY-MM-DD")};
+
+        this.handleChangeOfTask = this.handleChangeOfTask.bind(this);
+        this.handleChangeOfDate = this.handleChangeOfDate.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeOfTask(event) {
+        this.setState({task: event.target.value});
+    }
+
+    handleChangeOfDate(event) {
+        this.setState({date: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert(this.state.task + " was executed at " + this.state.date);
+        event.preventDefault();
+    }
 
     render() {
         return (
             <div className="add-execution">
-                Add execution
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="task">What did you do?</label>
+                        <select className="form-control"
+                                id="task"
+                                value={this.state.task}
+                                onChange={this.handleChangeOfTask}>
+                            <option value="grapefruit">Grapefruit</option>
+                            <option value="lime">Lime</option>
+                            <option value="coconut">Coconut</option>
+                            <option value="mango">Mango</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="date">When did you do it?</label>
+                        <input type="date"
+                               id="date"
+                               className="form-control"
+                               value={this.state.date} onChange={this.handleChangeOfDate} />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Submit" />
+                </form>
             </div>
         );
     };
