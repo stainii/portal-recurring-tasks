006cf0949a7dd745f64a5e756bb9dd68a082e029
stijnhooft@hotmail.com <stijnhoof@hotmail.com>
2018-06-11 15:45:09 +0200

    Fix publish list of events (temporary, should be improved)

--- src/main/java/be/stijnhooft/portal/housagotchi/routes/EventRoute.java
+++ src/main/java/be/stijnhooft/portal/housagotchi/routes/EventRoute.java
@@ -1,7 +1,14 @@
 package be.stijnhooft.portal.housagotchi.routes;
 
+import be.stijnhooft.portal.model.domain.Event;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.component.jackson.ListJacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,10 +16,16 @@ public class EventRoute extends RouteBuilder {
 
     @Override
     public void configure() {
+        //TODO: make this sharable config
+        JacksonDataFormat formatForListOfEvents = new ListJacksonDataFormat(Event.class);
+        formatForListOfEvents.addModule(new ParameterNamesModule());
+        formatForListOfEvents.addModule(new Jdk8Module());
+        formatForListOfEvents.addModule(new JavaTimeModule());
+        formatForListOfEvents.disableFeature(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         from("direct:eventService")
-                .marshal()
-                .json(JsonLibrary.Jackson)
-                .to("log:sending-out-event?level=INFO")
+                .marshal(formatForListOfEvents)
+                .setExchangePattern(ExchangePattern.InOnly)
                 .to("activemq:topic:EventTopic?timeToLive=3600000");
     }
 
