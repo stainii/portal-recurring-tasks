0e5012e6110381291c65272d4eaa24cbab964c42
stainii <stijnhooft@hotmail.com>
2019-11-20 11:03:51 +0100

    some cleanup

--- src/test/java/be/stijnhooft/portal/housagotchi/schedulers/PublishOvertimeRecurringTasksTest.java
+++ src/test/java/be/stijnhooft/portal/housagotchi/schedulers/PublishOvertimeRecurringTasksTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,7 +22,7 @@ import java.util.stream.Stream;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PublishOvertimeRecurringTasksTest {
 
     @InjectMocks
